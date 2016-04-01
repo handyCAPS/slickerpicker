e78961ce31320b178e4750e9c236341247bfbaa1
Tim Doppenberg <info@timdoppenberg.nl>
2016-04-01 17:21:19 +0200

    first commit

---
+++ Gruntfile.js
@@ -0,0 +1,71 @@
+/*global module:false*/
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    // Metadata.
+    pkg: grunt.file.readJSON('package.json'),
+    banner: '/*! <%= pkg.name %> - v<%= pkg.version %> - ' +
+      '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
+      '<%= pkg.homepage ? "* " + pkg.homepage + "\\n" : "" %>' +
+      '* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author %>;' +
+      ' Licensed <%= pkg.license %> */\n',
+    uglify: {
+      options: {
+        banner: '<%= banner %>'
+      },
+      dist: {
+        src: '<%= concat.dist.dest %>',
+        dest: '<%= Path.js.dist + "/" + pkg.name %>.min.js'
+      }
+    },
+    jshint: {
+      options: {
+        devel: true,
+        curly: true,
+        eqeqeq: true,
+        immed: true,
+        latedef: true,
+        newcap: true,
+        noarg: true,
+        sub: true,
+        undef: true,
+        unused: true,
+        boss: true,
+        eqnull: true,
+        browser: true,
+        globals: {
+          require: true
+        }
+      },
+      gruntfile: {
+        src: 'Gruntfile.js'
+      },
+      dev_test: {
+        src: [ '**/*.js' ]
+      dist_test: {
+        src: [ '**/*.min.js']
+      }
+    },
+    watch: {
+      options: {
+        livereload: true
+      },
+      gruntfile: {
+        files: '<%= jshint.gruntfile.src %>',
+        tasks: ['jshint:gruntfile']
+      },
+      js: {
+        files: [ '**/*.js'],
+        tasks: ['jshint']
+      }
+    }
+  });
+
+  // These plugins provide necessary tasks.
+  require('load-grunt-tasks')(grunt);
+
+  // Default task.
+  grunt.registerTask('default', ['jshint', 'uglify']);
+
+};
