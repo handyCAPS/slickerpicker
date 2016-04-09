88825cdf7b32176dd41fe8cb1ba35216aa6b8a0d
Tim Doppenberg <info@timdoppenberg.nl>
2016-04-09 23:02:06 +0200

    addlisteners

--- js/slickerpicker.js
+++ js/slickerpicker.js
@@ -3,24 +3,45 @@ function get(el) {
     return document['querySelector' + (el.indexOf('#') === 0 ? '' : 'All')](el);
 }
 
+function findParent(node, type) {
+    var tag = node.tagName.toLowerCase();
+    console.log(tag);
+    if (tag === type.toLowerCase()) { return node; }
+    if (!node.parent) { return null; }
+    return findParent(node.parent, type);
+}
 
 
-var SlickerPicker = function(element, options) {
+
+var SlickerPicker = function(linkedInput, options) {
 
     "use strict";
 
-    if (!element || element.tagName !== 'INPUT') {
-        console.error('Element is not an input. ' + element);
+    if (!linkedInput || linkedInput.tagName !== 'INPUT') {
+        console.error('Element is not an input. ' + linkedInput);
         return;
     }
 
     var Classes = {
         base: 'SlickerPicker',
+        wrapper: 'wrapper',
         table: 'table',
         day: 'day',
         dayN: 'day-',
         header: 'header'
     };
+
+    function getClass(type, withPoint) {
+        var point = withPoint ? '.' : '';
+        return point + Classes['base'] + '-' + Classes[type];
+    }
+
+    var otherPickers = get(getClass('wrapper', true));
+    var spid = Classes['base'] + (otherPickers === undefined ? 0 : otherPickers.length);
+
+    var parentNode = linkedInput.parentNode;
+
+    var openTable = null;
 
     var daysOfWeek = {
         nl: [
@@ -34,21 +55,27 @@ var SlickerPicker = function(element, options) {
         ]
     };
 
-    function getClass(type, withPoint) {
-        var point = withPoint ? '.' : '';
-        return point + Classes['base'] + '-' + Classes[type];
-    }
-
     var Input = (function() {
-        var listener = function() {
-            console.log('Listening');
+        var inListener = function() {
+            Table.insertTable();
         };
-        function listen(stop) {
-            element[['add', 'remove'][!!stop * 1] + 'EventListener']('focus', listener);
+        var outListener = function(event) {
+            var clickedElement = event.relatedTarget;
+            // var parentDiv = findParent(clickedElement, 'div');
+            // var closestPa = Element.closest.call(clickedElement, getClass('wrapper', true));
+            console.log(openTable);
+            Table.toggleTableVis(true);
+        };
+        function listenIn(stop) {
+            linkedInput[['add', 'remove'][!!stop * 1] + 'EventListener']('focus', inListener);
+        }
+        function listenOut(stop) {
+            linkedInput[['add', 'remove'][!!stop * 1] + 'EventListener']('blur', outListener);
         }
 
         return {
-            listen: listen
+            listenIn: listenIn,
+            listenOut: listenOut
         };
 
     }());
@@ -57,9 +84,11 @@ var SlickerPicker = function(element, options) {
         function createRow() {
             return document.createElement('tr');
         }
+
         function createCell() {
             return document.createElement('td');
         }
+
         function createTableHeader() {
             var header = createRow();
             for (var i = 0; i < 7; i++) {
@@ -70,6 +99,7 @@ var SlickerPicker = function(element, options) {
             }
             return header;
         }
+
         function createTable(days) {
             var table = document.createElement('table');
             table.classList.add(getClass('table'));
@@ -89,6 +119,24 @@ var SlickerPicker = function(element, options) {
             return table;
         }
 
+        function createWrapper () {
+            var wrapper = document.createElement('div');
+            wrapper.classList.add(getClass('wrapper'));
+            wrapper.id = spid;
+            return wrapper;
+        }
+
+        function insertTable() {
+            if (openTable === null) {
+                var wrapper = createWrapper();
+                wrapper.appendChild(createTable(31));
+                parentNode.insertBefore(wrapper, linkedInput.nextSibling);
+                openTable = wrapper;
+            } else {
+                toggleTableVis();
+            }
+        }
+
         function blankDay(day) {
             var cell = get(getClass('dayN', true) + day)[0];
             cell.classList.add('hidden');
@@ -96,17 +144,22 @@ var SlickerPicker = function(element, options) {
             cell.style.pointerEvents = 'none';
         }
 
+        function toggleTableVis(off) {
+            if (openTable !== null) {
+                openTable.hidden = !!off;
+            }
+        }
+
         return {
-            createTable: createTable,
-            blankDay: blankDay
+            insertTable: insertTable,
+            blankDay: blankDay,
+            toggleTableVis: toggleTableVis
         };
     }());
 
-    document.body.appendChild(Table.createTable(31));
 
-    Table.blankDay(4);
-
-    Input.listen();
+    Input.listenIn();
+    Input.listenOut();
 
 };
 
