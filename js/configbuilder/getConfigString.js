63f753c51c26122c818837078aa91cd48ea56c54
Tim Doppenberg <info@timdoppenberg.nl>
2016-05-26 10:00:51 +0200

    getConfigString does single level

--- js/configbuilder/getConfigString.js
+++ js/configbuilder/getConfigString.js
@@ -3,8 +3,37 @@ const axis = require('axis.js/dist/axis.js');
 
 function getConfigString(configArray) {
 
-    let resultString = '';
+    const tabSpace = '    ';
 
+    function tabSpaceN(n) {
+        let result = '';
+        for (let i = 0; i < n; i++) {
+            result += tabSpace;
+        }
+        return result;
+    }
+
+    let resultString = 'var config = {\n';
+
+    resultString += tabSpace;
+
+    resultString += configArray
+                    .map( value => {
+                        if (Array.isArray(value)) {
+                            return value[0] +
+                                ': {\n' +
+                                tabSpaceN(2) +
+                                value[1]
+                                    .join(',\n' + tabSpace) +
+                                '\n' +
+                                tabSpace +
+                                '}';
+                        }
+                        return value;
+                    })
+                    .join(',\n' + tabSpace);
+
+    resultString += '\n};';
 
     return resultString;
 }
--- test/getConfigString_spec.js
+++ test/getConfigString_spec.js
@@ -1,13 +1,43 @@
 
 import {expect} from 'chai';
 
+import objectToArray from '../js/configbuilder/objectToArray.js';
+
 import getConfigString from '../js/configbuilder/getConfigString.js';
 
 describe('Testing getConfigString', () => {
 
+    const stringOpening = "var config = {\n    ";
+
+    const stringClosing = "\n};";
+
     it('should return a string', () => {
         const actual = getConfigString(['prop: value']);
         expect(typeof actual).to.equal('string');
+    });
+
+    it('should start with var config and end with a semicolon', () => {
+        const actual = getConfigString(['prop: value']);
+        expect(actual).to.match(/^var config = \{\n\s{4}.*\n\};$/g);
+    });
+
+    it('should return a config object from an array', () => {
+        const actual = getConfigString(["prop: 'value'"]);
+        const expected = stringOpening + "prop: 'value'" + stringClosing;
+        expect(actual).to.equal(expected);
+    });
+
+    it('should handle multiple properties', () => {
+        const actual = getConfigString(['prop1: value1', 'prop2: value2']);
+        const expected = stringOpening + 'prop1: value1,\n    prop2: value2' + stringClosing;
+        expect(actual).to.equal(expected);
+    });
+
+    it('should handle nested properties', () => {
+        const nestedArray = [['prop1', ['nestedProp: nestedValue']]];
+        const actual = getConfigString(nestedArray);
+        const expected = stringOpening + "prop1: {\n        nestedProp: nestedValue\n    }" + stringClosing;
+        expect(actual).to.equal(expected);
     });
 
 });
